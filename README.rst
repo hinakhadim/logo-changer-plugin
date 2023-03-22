f450c195c7fabf0701233e31866b94383345d6d0
Hina Khadim <hina.khadim@edly.io>
2023-03-22 17:21:46 +0500

    init: Initial commit

---
+++ README.rst
@@ -0,0 +1,22 @@
+logo-changer plugin for `Tutor <https://docs.tutor.overhang.io>`__
+===================================================================================
+
+Installation
+------------
+
+::
+
+    pip install git+https://github.com/hinakhadim/logo-changer-plugin.git
+
+Usage
+-----
+
+::
+
+    tutor plugins enable logo-changer
+
+
+License
+-------
+
+This software is licensed under the terms of the AGPLv3.
---
+++ setup.py
@@ -0,0 +1,59 @@
+import io
+import os
+from setuptools import setup, find_packages
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+
+def load_readme():
+    with io.open(os.path.join(HERE, "README.rst"), "rt", encoding="utf8") as f:
+        return f.read()
+
+
+def load_about():
+    about = {}
+    with io.open(
+        os.path.join(HERE, "tutorlogo_changer", "__about__.py"),
+        "rt",
+        encoding="utf-8",
+    ) as f:
+        exec(f.read(), about)  # pylint: disable=exec-used
+    return about
+
+
+ABOUT = load_about()
+
+
+setup(
+    name="tutor-contrib-logo-changer",
+    version=ABOUT["__version__"],
+    url="https://github.com/hinakhadim/logo-changer-plugin.git",
+    project_urls={
+        "Code": "https://github.com/hinakhadim/logo-changer-plugin.git",
+        "Issue tracker": "https://github.com/hinakhadim/logo-changer-plugin.git/issues",
+    },
+    license="AGPLv3",
+    author="Hina Khadim",
+    description="logo-changer plugin for Tutor",
+    long_description=load_readme(),
+    packages=find_packages(exclude=["tests*"]),
+    include_package_data=True,
+    python_requires=">=3.7",
+    install_requires=["tutor"],
+    entry_points={
+        "tutor.plugin.v1": [
+            "logo-changer = tutorlogo_changer.plugin"
+        ]
+    },
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: GNU Affero General Public License v3",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+    ],
+)
